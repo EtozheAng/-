3695ff0eda60589c3bd206b6e28f31db7606074d
EtozheAng <pavel.khisamutdinov@yandex.ru>
2022-04-02 18:33:14 +0300

    main part completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,9 @@
  $(function(){
-   $(".menu__sub").hide(); // скрываем выпадающее меню
-   $(".menu li:has('.menu__sub')").hover(
-  function(){
-       $(".menu__sub").stop().fadeToggle(300);} /* отбираем элемент списка, который содержит элемент с классом .submenu и добавляем ему функцию при наведении, которая показывает и скрывает выпадающее меню */
-);
+//    $(".menu__sub").hide(); // скрываем выпадающее меню
+//    $(".menu li:has('.menu__sub')").hover(
+//   function(){
+//        $(".menu__sub").stop().fadeToggle(300);} /* отбираем элемент списка, который содержит элемент с классом .submenu и добавляем ему функцию при наведении, которая показывает и скрывает выпадающее меню */
+// );
    $('.slider__items').slick({
      prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
      nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
@@ -12,5 +12,10 @@
      slidesToScroll: 2,
      dots: true,
      variableWidth: true
+   });
+
+   $('.accordion-item').click(function(){
+     $(this).toggleClass('accordion-item__title--active');
+     $(this).children('.accordion-item__content').slideToggle(400);
    });
  });
